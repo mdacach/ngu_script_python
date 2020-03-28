566ec0f926b02d7aa18f04a61481f02703fb80f7
matheus <dacachmatheus@gmail.com>
2020-03-28 17:47:54 -0300

    refactor to use classes

--- script.py
+++ script.py
@@ -26,68 +26,72 @@ def click(x, y, button="left"):
 ########################################
 
 
+f = Features()
+i = Inventory()
+
+
 def run15():
     start = time.time()
     print("start")
-    handleTraining()
-    fightBosses()
-    farmAdventure()
+    f.basicTraining()
+    f.fightBosses()
+    f.adjustAdventureZone()
     print('basic loop for 30 times')
     counter = 0
     while ((time.time() - start)/60 < 13):
-        adventure(30)
+        f.farmAdventure(30)
         reclaimEnergy()
-        handleTraining()
-        handleAugments()
-        fightBosses()
+        f.basicTraining()
+        f.augmentation()
+        f.fightBosses()
         if (counter % 5 == 0):
-            handleEquips()
+            i.handleEquips()
         counter += 1
     print('last part')
     reclaimEnergy()
-    augmentationUpgrade()
-    adventure(30)
-    fightBosses()
-    moneyPit()
+    f.augmentation(upgrade=True)
+    f.farmAdventure(30)
+    f.fightBosses()
+    f.moneyPit()
     while ((time.time() - start)/60 < 15):
         pyautogui.sleep(5)
-    rebirth()
+    f.rebirth()
     print('end')
 
 
 def run30():
     print('basic loop 3 times')
     for i in range(3):
-        handleTraining()
-        fightBosses()
-        farmAdventure()
-        handleEquips()
+        f.basicTraining()
+        f.fightBosses()
+        f.adjustAdventureZone()
+        i.handleEquips()
         print('handle training')
         for i in range(3):
             print(f'loop {i+1}')
             pyautogui.sleep(30)
             reclaimEnergy()
-            handleTraining()
-            handleAugments()
-        adventure(180)
+            f.basicTraining()
+            f.augmentation()
+        f.farmAdventure(180)
         reclaimEnergy()
-        handleTraining()
-        handleAugments()
-        fightBosses()
-        adventure(180)
+        f.basicTraining()
+        f.augmentation()
+        f.fightBosses()
+        f.farmAdventure(180)
     print('final part')
     reclaimEnergy()
-    handleTraining()
+    f.basicTraining()
     reclaimEnergy()
-    handleAugments()
-    adventure(120)
+    f.augmentation()
+    f.farmAdventure(120)
     reclaimEnergy()
-    augmentationUpgrade()
-    adventure(60)
-    handleEquips()
-    fightBosses()
-    moneyPit()
-    rebirth()
+    f.augmentation(upgrade=True)
+    f.farmAdventure(60)
+    i.handleEquips()
+    f.fightBosses()
+    f.moneyPit()
+    f.rebirth()
 
 
 def main():
@@ -112,133 +116,126 @@ def main():
             print()
     if choice == "2":
         print('farming script')
+        start = time.time()
+        counter = 0
         while True:
             farming()
+            counter += 1
+            if (counter % 10):
+                print(f'time elapsed: {round((time.time() - start)/60)}')
+
+
+class Features:
+
+    def basicTraining(self):
+        click(*BASIC_TRAINING)
+        click(*ATK1)
+        click(*DEF1)
+        click(*ATK2)
+        click(*DEF2)
+        click(*ATK3)
+        click(*DEF3)
+        click(*ATK4)
+        click(*DEF4)
+        click(*DEF5)
+        click(*ATK5)
+
+    def moneyPit(self):
+        click(*MONEY_PIT)
+        click(*FEED_ME)
+        click(*FEED_YEAH)
+
+    def rebirth(self):
+        click(*REBIRTH_MENU)
+        pyautogui.sleep(5)
+        print("going to rebirth")
+        click(*REBIRTH_BUTTON)
+        click(*REBIRTH_CONFIRMATION)
+        print("rebirth")
+
+    def fightBosses(self):
+        click(*FIGHT_BOSS)
+        click(*NUKE)
+        for i in range(5):
+            pyautogui.sleep(2)
+            click(*FIGHT)
+
+    def adjustAdventureZone(self):
+        click(*ADVENTURE)
+        click(*ADVANCE_ZONE, button="right")
+
+    def farmAdventure(self, time):
+        adjustAdventureZone()
+        pyautogui.press('q')
+        for i in range(time):
+            pyautogui.press('y')
+            pyautogui.press('t')
+            pyautogui.press('r')
+            pyautogui.press('e')
+            pyautogui.press('w')
+            pyautogui.sleep(1)
+        pyautogui.press('q')
+
+    def augmentation(self, upgrade=false):
+        click(*AUGMENTATION)
+        if (upgrade):
+            click(*AUG1_UPGRADE)
+        else:
+            click(*AUG1)
+
+
+class Inventory:
+    def mergeItem(self, x, y):
+        global CORNER
+        pyautogui.moveTo(CORNER[0] + x, CORNER[1] + y - 25)
+        pyautogui.press('d')
+
+    def boostItem(self, x, y):
+        global CORNER
+        pyautogui.moveTo(CORNER[0] + x, CORNER[1] + y - 25)
+        pyautogui.press('a')
+
+    def handleEquips(self):
+        click(*INVENTORY)
+        mergeItem(*HEAD)
+        mergeItem(*CHEST)
+        mergeItem(*LEGS)
+        mergeItem(*BOOTS)
+        mergeItem(*WEAPON)
+        mergeItem(*ACC1)
+        mergeItem(*ACC2)
+        boostItem(*HEAD)
+        boostItem(*CHEST)
+        boostItem(*LEGS)
+        boostItem(*BOOTS)
+        boostItem(*WEAPON)
+        boostItem(*ACC1)
+        boostItem(*ACC2)
+
+        x = SLOT1[0]
+        y = SLOT1[1]
+        # merge and boost front row
+        for i in range(12):
+            mergeItem(x + INV_DIFF * i, y)
+            boostItem(x + INV_DIFF * i, y)
+
+        # boost infinity cube
+        click(*CUBE, button="right")
+
+        # for i in range(12):
+        #     for j in range(3):
+        #         mergeItem(x + INV_DIFF * i, y + INV_DIFF * j)
+        #         boostItem(x + INV_DIFF * i, y + INV_DIFF * j)
 
 
 def farming():
-    adventure(60)
-    handleEquips()
+    f.farmAdventure(60)
+    i.handleEquips()
 
 
 def reclaimEnergy():
     click(*BASIC_TRAINING)
     pyautogui.press('r')  # should reclaim energy
-
-
-def augmentationUpgrade():
-    click(*AUGMENTATION)
-    click(*AUG1_UPGRADE)
-
-
-def moneyPit():
-    click(*MONEY_PIT)
-    click(*FEED_ME)
-    click(*FEED_YEAH)
-
-
-def rebirth():
-    click(*REBIRTH_MENU)
-    pyautogui.sleep(20)
-    print("going to rebirth")
-    click(*REBIRTH_BUTTON)
-    click(*REBIRTH_CONFIRMATION)
-    print("rebirth")
-
-
-def saveScreenshot():
-    img = pyautogui.screenshot()
-    img.save(r'rebirth-screen.png')
-
-
-def adventure(time):
-    farmAdventure()
-    pyautogui.press('q')
-    for i in range(time):
-        pyautogui.press('y')
-        pyautogui.press('t')
-        pyautogui.press('r')
-        pyautogui.press('e')
-        pyautogui.press('w')
-        pyautogui.sleep(1)
-    pyautogui.press('q')
-
-
-def handleTraining():
-    click(*BASIC_TRAINING)
-    click(*ATK1)
-    click(*DEF1)
-    click(*ATK2)
-    click(*DEF2)
-    click(*ATK3)
-    click(*DEF3)
-    click(*ATK4)
-    click(*DEF4)
-    click(*DEF5)
-    click(*ATK5)
-
-
-def handleAugments():
-    click(*AUGMENTATION)
-    click(*AUG1)
-
-
-def fightBosses():
-    click(*FIGHT_BOSS)
-    click(*NUKE)
-    for i in range(5):
-        pyautogui.sleep(2)
-        click(*FIGHT)
-
-
-def farmAdventure():
-    click(*ADVENTURE)
-    click(*ADVANCE_ZONE, button="right")
-
-
-def handleEquips():
-    click(*INVENTORY)
-    mergeItem(*HEAD)
-    mergeItem(*CHEST)
-    mergeItem(*LEGS)
-    mergeItem(*BOOTS)
-    mergeItem(*WEAPON)
-    mergeItem(*ACC1)
-    mergeItem(*ACC2)
-    boostItem(*HEAD)
-    boostItem(*CHEST)
-    boostItem(*LEGS)
-    boostItem(*BOOTS)
-    boostItem(*WEAPON)
-    boostItem(*ACC1)
-    boostItem(*ACC2)
-
-    x = SLOT1[0]
-    y = SLOT1[1]
-    # merge and boost front row
-    for i in range(12):
-        mergeItem(x + INV_DIFF * i, y)
-        boostItem(x + INV_DIFF * i, y)
-
-    # boost infinity cube
-    click(*CUBE, button="right")
-
-    # for i in range(12):
-    #     for j in range(3):
-    #         mergeItem(x + INV_DIFF * i, y + INV_DIFF * j)
-    #         boostItem(x + INV_DIFF * i, y + INV_DIFF * j)
-
-
-def mergeItem(x, y):
-    global CORNER
-    pyautogui.moveTo(CORNER[0] + x, CORNER[1] + y - 25)
-    pyautogui.press('d')
-
-
-def boostItem(x, y):
-    global CORNER
-    pyautogui.moveTo(CORNER[0] + x, CORNER[1] + y - 25)
-    pyautogui.press('a')
 
 
 if __name__ == "__main__":
