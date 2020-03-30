20de54ded00c8e70918abc8ed1870ee3b0d63d2f
matheus <dacachmatheus@gmail.com>
2020-03-30 13:01:33 -0300

    add helper module

---
+++ features.py
@@ -0,0 +1,4 @@
+""" Module for various features handling."""
+
+from helper import *
+from coords import *
---
+++ helper.py
@@ -0,0 +1,21 @@
+""" Helper functions. """
+
+import pyautogui
+# get the game corner coordinates
+CORNER = pyautogui.locateOnScreen('ingame-corner.png')
+
+# our coordinates are shifted 25 px up because of steam border
+
+# lower pyautogui.PAUSE constant for more efficiency
+pyautogui.PAUSE = 0.01
+
+
+def moveTo(x, y):
+    x = CORNER[0] + x
+    y = CORNER[1] + y - 25
+    pyautogui.moveTo(x, y)
+
+
+def click(x, y, button="left"):
+    moveTo(x, y)
+    pyautogui.click()
