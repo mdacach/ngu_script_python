0d4c30c911c30e22cc131af5f9593d595a724622
matheus <dacachmatheus@gmail.com>
2020-04-04 11:51:27 -0300

    add docstrings to helper functions

--- helper.py
+++ helper.py
@@ -1,4 +1,7 @@
-""" Helper functions. """
+""" Helper functions. 
+
+    Intializes by getting position of top-left corner of the game. 
+"""
 
 import pyautogui
 # get the game corner coordinates
@@ -6,6 +9,8 @@ CORNER = None
 print('searching for corner...')
 while CORNER == None:
     CORNER = pyautogui.locateOnScreen('ingame-corner.png')
+    if CORNER == None:
+        print('could not find top-left corner')
 print('success')
 # our coordinates are shifted 25 px up because of steam border
 
@@ -14,31 +19,36 @@ pyautogui.PAUSE = 0.01
 
 
 def getCoords(x, y):
+    """ Return coordinates relative to top-left corner. """
     return (CORNER[0] + x, CORNER[1] + y - 25)
 
 
 def moveTo(x, y):
+    """ Move mouse to coordinates x, y in-game. """
     x = CORNER[0] + x
     y = CORNER[1] + y - 25
     pyautogui.moveTo(x, y)
 
 
 def rawMove(x, y):
+    """ Move mouse to absolute coordinates x, y. """
     pyautogui.moveTo(x, y)
 
 
 def rawClick(x, y, button="left"):
+    """ Click on absolute coordinates x, y. """
     pyautogui.click(x, y, button=button)
 
 
 def click(x, y, button="left"):
+    """ Click on coordinates x, y relative to top-left corner. """
     moveTo(x, y)
     pyautogui.click(button=button)
     sleep(0.2)
 
 
 def ctrlClick():
-    """ Performs a control click on current mouse position"""
+    """ Control click on current mouse position. """
     pyautogui.keyDown('ctrl')
     sleep(0.3)
     pyautogui.click()
@@ -47,4 +57,5 @@ def ctrlClick():
 
 
 def sleep(time):
+    """ Sleeps for {time} amount of seconds. """
     pyautogui.sleep(time)
