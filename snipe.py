4296b8e6a9118b672b25f971ce64b13cedae6501
matheus <dacachmatheus@gmail.com>
2020-04-19 10:41:01 -0300

    updated snipe script

--- snipe.py
+++ snipe.py
@@ -19,20 +19,17 @@ print(f'sniping {args.zone}')
 start = time.time()
 killCounter = 0
 print('buffing')
-Adventure.adventureZone('safe')
-Adventure.buff()
 Adventure.adventureZone(args.zone)
 killed = False  # kill flag to only take action after kills
 while True:
     if Adventure.enemySpawn() and Adventure.isBoss():
-        Adventure.kill()
+        Adventure.kill(buffs=True)
         sleep(1)
         press('w')
         killed = True
         Adventure.adventureZone('safe')
         while not Adventure.isPlayerFull():
             sleep(1)
-        Adventure.buff()
         Adventure.adventureZone(args.zone)
     elif Adventure.enemySpawn() and not Adventure.isBoss():
         Adventure.refreshZone()
