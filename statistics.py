f981eb0b5dcf8b98eb1c0f3a6045c3f238c8d1b7
matheus <dacachmatheus@gmail.com>
2020-04-21 21:18:07 -0300

    numpy and opencv image processing stuff (EXPERIMENTAL)

--- statistics.py
+++ statistics.py
@@ -1,6 +1,7 @@
 """ Statistics module. """
 import pyautogui
 import d3dshot
+import cv2
 from helper import *
 from coords import *
 import pytesseract as ocr
@@ -58,18 +59,18 @@ class Statistics:
 
     @staticmethod
     def getPP():
-        """ Get the current pp amount from itopod. 
+        """ Get the current pp amount from itopod.
 
-        Uses d3dshot for screenshot. 
+        Uses d3dshot for screenshot.
         """
         Navigation.menu('adventure')
         click(*ITOPOD_PERKS)
         x, y = getCoords(ITOPOD_PP_REGION[0], ITOPOD_PP_REGION[1])
         d = d3dshot.create()
         image = d.screenshot(region=(x, y, x+100, y+30))
-        print(image)
-        image = d.screenshot_to_disk(
-            file_name="pp-screenshot.png", region=(x, y, x+100, y+30))
+        # print(image)
+        # image = d.screenshot_to_disk(
+        #     file_name="pp-screenshot.png", region=(x, y, x+100, y+30))
         try:
             text = ocr.image_to_string(image)
         except:
@@ -88,3 +89,45 @@ class Statistics:
             return -1
         # return exp
         # return f"{exp} exp"
+
+    @staticmethod
+    def getTierKills():
+        """ Get and return itopod tier remaining kills to AP.
+
+        Must be in ITOPOD menu.
+        """
+        # ONLY WORDS FOR TIERS ABOVE 150
+        click(*ITOPOD_CLICK_TOOLTIP)
+        x, y = getCoords(
+            ITOPOD_TIER_COUNT_REGION[0], ITOPOD_TIER_COUNT_REGION[1])
+        d = d3dshot.create(capture_output='numpy')
+        # image = d.screenshot_to_disk(
+        # file_name = "itopod-tier.png", region = (x, y, x + 77, y + 18))
+        print(f"x: {x} y: {y}")
+        # image = d.screenshot(region=(x, y, x + 77, y + 18))
+        image = d.screenshot()
+        # print(f'image before: {image}')
+        # image = image.resize((50, 35))
+        image = cv2.resize(image, None, fx=1.5, fy=1.5,
+                           interpolation=cv2.INTER_CUBIC)
+        # image.save('itopod-ss.png')
+        print(f'image after: {image}')
+        try:
+            text = ocr.image_to_string(image)
+        except:
+            print('could not read itopod tier count')
+            text = 0
+        count = ""
+        for letter in text:
+            if str.isdigit(letter):
+                count += letter
+        print(f'read {text}')
+        try:
+            return int(count)
+        except:
+            print('could not read itopod tier count properly. returning 40')
+            return -1
+
+
+# (511, 49) -> (537, 142)
+# # region (77, 20)
