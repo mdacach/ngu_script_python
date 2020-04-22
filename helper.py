64b392461b936e0e798cf32bcd4f19ef130fab07
matheus <dacachmatheus@gmail.com>
2020-04-21 22:28:51 -0300

    add type hinting

--- helper.py
+++ helper.py
@@ -3,8 +3,11 @@
     Intializes by getting position of top-left corner of the game. 
 """
 
-import pyautogui
 import d3dshot
+import pyautogui
+
+import constants
+
 # get the game corner coordinates
 CORNER = None
 print('searching for corner...')
@@ -19,49 +22,57 @@ print('success')
 pyautogui.PAUSE = 0.01
 
 
-def sleep(time):
-    """ Sleeps for {time} amount of seconds. """
+def sleep(time: int) -> None:
+    """ Sleep for x amount of seconds. """
     pyautogui.sleep(time)
 
 
-def getCoords(x, y):
-    """ Return coordinates relative to top-left corner. """
+def getCoords(x: int, y: int) -> Tuple[int]:
+    """ Return coordinates relative to top-left corner of the game. """
     return (CORNER[0] + x, CORNER[1] + y - 25)
 
 
-def moveTo(x, y):
-    """ Move mouse to coordinates x, y in-game. """
-    x = CORNER[0] + x
-    y = CORNER[1] + y - 25
+def moveTo(x: int, y: int) -> None:
+    """ Move mouse to coordinates x, y relative to game. """
+    x, y = getCoords(x, y)
     pyautogui.moveTo(x, y)
 
 
-def rawMove(x, y):
-    """ Move mouse to absolute coordinates x, y. """
+def rawMove(x: int, y: int) -> None:
+    """ Move mouse to absolute coordinates x, y. 
+
+    This method does not use getCoords.
+    """
     pyautogui.moveTo(x, y)
 
 
-def rawClick(x, y, button="left"):
-    """ Click on absolute coordinates x, y. """
+def rawClick(x: int, y: int, button: str = "left") -> None:
+    """ Click on absolute coordinates x, y. 
+
+    Keyword arguments:  
+    button -- left or right.
+    """
     pyautogui.click(x, y, button=button)
 
 
-def click(x, y, button="left", delay="medium"):
-    """ Click on coordinates x, y relative to top-left corner. 
+def click(x: int, y: int, button: str = "left", delay: str = "medium") -> None:
+    """ Click on coordinates x, y relative to game. 
 
     Keyword arguments:  
-    button -- button to click. {left, right}  
-    delay -- time to wait after click {fast, medium}.  
+    button -- left or right.  
+    delay -- time to wait after click: fast, medium or long.  
     """
     moveTo(x, y)
     pyautogui.click(button=button)
-    if delay == "medium":
-        sleep(0.3)
-    elif delay == "fast":
-        sleep(0.1)
+    if delay == "fast":
+        sleep(constants.FAST_SLEEP)
+    elif delay == "medium":
+        sleep(constants.MEDIUM_SLEEP)
+    elif delay == "long":
+        sleep(constants.LONG_SLEEP)
 
 
-def ctrlClick():
+def ctrlClick() -> None:  # TODO add coordinates
     """ Control click on current mouse position. """
     pyautogui.keyDown('ctrl')
     sleep(0.3)
@@ -70,7 +81,7 @@ def ctrlClick():
     pyautogui.keyUp('ctrl')
 
 
-def press(letters, delay=0):
+def press(letters: str, delay: int = 0) -> None:
     """ Send letters to window. 
 
     Keyword arguments:  
@@ -82,7 +93,7 @@ def press(letters, delay=0):
             pyautogui.sleep(delay)
 
 
-def getScreenshot2():
+def getScreenshot2():  # TODO
     """ Get and return screenshot of actual screen. 
 
     Uses d3dshot module instead of pyautogui. 
@@ -91,7 +102,7 @@ def getScreenshot2():
     return d.screenshot()
 
 
-def getScreenshot(region=None):
+def getScreenshot(region: Tuple[int, int, int, int] = None) -> None:
     """ Get screenshot of actual screen. 
 
     To be used with get pixel color when you want to get multiple pixels at the same time.
@@ -104,11 +115,12 @@ def getScreenshot(region=None):
     return pyautogui.screenshot()
 
 
-def getPixelColor(x, y, image=None):
+def getPixelColor(x: int, y: int, image: Image = None) -> None:
     """ Get color of pixel at (x, y) (relative). 
 
     Keyword arguments:  
-    image -- screenshot, if you want to get multiple pixels. """
+    image -- screenshot, if you want to get multiple pixels with the same image. 
+    """
     pix = getCoords(x, y)
     if image:
         return image.getpixel(pix)
