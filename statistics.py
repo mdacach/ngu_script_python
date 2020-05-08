b400d22e60d4b0cc7e664d6526ae8b32494457b4
matheus <dacachmatheus@gmail.com>
2020-05-08 17:55:48 -0300

    fix ocr (now with pyautogui ss)

--- statistics.py
+++ statistics.py
@@ -29,11 +29,21 @@ class Statistics:
             region = getRegion(*region)
         else:  # all game screen
             region = (CORNER[0], CORNER[1],
-                      GAME_WIDTH, GAME_HEIGHT)
+                      coords.GAME_WIDTH, coords.GAME_HEIGHT)
         # print(f'region: {region}')
         if save:
             pyautogui.screenshot("output/" + name, region=region)
         return pyautogui.screenshot(region=region)
+
+    @staticmethod 
+    def removeLetters(text: str) -> int:
+        """ Remove letters from string for OCR.  
+        
+        Keyword arguments:  
+        text -- the text to use.  
+        """ 
+        text = [x for x in text if x.isdigit()]
+        return int("".join(text))
 
     @staticmethod
     def getEXP():
@@ -41,57 +51,22 @@ class Statistics:
         Navigation.menu('exp')
         img = Statistics.getScreenshot(region=coords.EXP_REGION)
         text = ocr.image_to_string(img)
-        text = [x for x in text.split() if x[0].isdigit()]
-        text = [x for x in text[0] if x.isdigit()]
-        return int("".join(text))
+        return Statistics.removeLetters(text)
 
 
     @staticmethod
-    def getBoss(): # TODO
-        """ Get the next boss from Fight Boss menu. """
-        x, y = getCoords(BOSS_NUMBER_REGION[0], BOSS_NUMBER_REGION[1])
-        img = pyautogui.screenshot(region=(x, y, 80, 20))
-        # img.save('exp-screenshot.png')
-        # print(ocr.image_to_string(img))
-        try:
-            text = ocr.image_to_string(img)
-        except:
-            print('could not read boss number')
-            return
-        print(text)
+    def getBoss() -> int:
+        """ Get and return the boss number from Fight Boss menu. """
+        img = Statistics.getScreenshot(save=True, region=coords.BOSS_NUMBER_REGION)
+        text = ocr.image_to_string(img)
+        return Statistics.removeLetters(text)
 
     @staticmethod
     def getPP(): # TODO 
-        """ Get the current pp amount from itopod.
-
-        Uses d3dshot for screenshot.
-        """
-        Navigation.menu('adventure')
-        click(*ITOPOD_PERKS)
-        x, y = getCoords(ITOPOD_PP_REGION[0], ITOPOD_PP_REGION[1])
-        d = d3dshot.create()
-        image = d.screenshot(region=(x, y, x+100, y+30))
-        # print(image)
-        # image = d.screenshot_to_disk(
-        #     file_name="pp-screenshot.png", region=(x, y, x+100, y+30))
-        try:
-            text = ocr.image_to_string(image)
-        except:
-            print('could not read pp')
-            text = 0
-        pp = ""
-        for letter in text:
-            if str.isdigit(letter):
-                pp += letter
-        # print(f"{exp} exp")
-        print(f'read {text}')
-        try:
-            return int(pp)
-        except:
-            print('could not return pp correctly')
-            return -1
-        # return exp
-        # return f"{exp} exp"
+        """ Get and return the current pp amount from itopod.  """ 
+        img = Statistics.getScreenshot(region=coords.ITOPOD_PP_REGION)
+        text = ocr.image_to_string(img)
+        return Statistics.removeLetters(text)
 
     @staticmethod
     def getTierKills(): # TODO 
@@ -100,47 +75,25 @@ class Statistics:
         Must be in ITOPOD menu.
         """
         # ONLY WORDS FOR TIERS ABOVE 150
-        click(*ITOPOD_CLICK_TOOLTIP)
-        x, y = getCoords(
-            ITOPOD_TIER_COUNT_REGION[0], ITOPOD_TIER_COUNT_REGION[1])
-        d = d3dshot.create(capture_output='numpy')
-        # image = d.screenshot_to_disk(
-        # file_name = "itopod-tier.png", region = (x, y, x + 77, y + 18))
-        print(f"x: {x} y: {y}")
-        # image = d.screenshot()
-        # print(f'image before: {image}')
-        # image = image.resize((50, 35))
-        # image.save('itopod-ss.png')
-        image = d.screenshot(region=(x, y, x + 77, y + 18))
-        image = cv2.resize(image, None, fx=1.5, fy=1.5,
-                           interpolation=cv2.INTER_CUBIC)
-        print(f'image after: {image}')
-        try:
-            text = ocr.image_to_string(image)
-        except:
-            print('could not read itopod tier count')
-            text = 0
-        count = ""
-        for letter in text:
-            if str.isdigit(letter):
-                count += letter
-        print(f'read {text}')
-        try:
-            return int(count)
-        except:
-            print('could not read itopod tier count properly. returning -1')
-            return -1
+        click(*coords.ITOPOD_CLICK_TOOLTIP)
+        img = Statistics.getScreenshot(region=coords.ITOPOD_TIER_COUNT_REGION)
+        text = ocr.image_to_string(img)
+        return Statistics.removeLetters(text)
 
 
 if __name__ == '__main__':
-    print(f'testing screenshot:')
-    # Statistics.getScreenshot(save=True)
-    # print(Statistics.getScreenshot(region=ITOPOD_PP_REGION))
-    for i in range(5):
-        start = time.time()
-        print(f'current exp: {Statistics.getEXP()}')
-        end = time.time()
-        print(f'time: {end - start}')
-        # img = Statistics.getScreenshot(region=EXP_REGION)
-        # print(img)
-        # print(ocr.image_to_string(img))
+    print(Statistics.getTierKills())
+    # N = 1 
+    # for i in range(N):
+    #     start = time.time() 
+    #     pyautogui.screenshot() 
+    #     end = time.time() 
+    # print(f'average time for pyautogui: {round((end - start)/N, 3)}')
+    # for i in range(N):
+    #     start = time.time() 
+    #     with mss() as sct:
+    #         sct.shot() 
+    #     end = time.time() 
+    # print(f'average time for mss: {round((end - start)/N, 3)}')
+
+
