dea9a2dfe9bfc63c2794cb1bb0058e6a371809a3
matheus <dacachmatheus@gmail.com>
2020-05-16 17:43:40 -0300

    experimental script with inv and ygg management

---
+++ itopod_experimental.py
@@ -0,0 +1,18 @@
+from inventory import invManagement
+from features import Adventure, Yggdrasil
+from navigation import Navigation
+
+
+totalEXP = 0 
+totalTime = 0 
+while True:
+    Navigation.menu('adventure')
+    print('*' * 30)
+    totalEXP += Adventure.itopodExperimental(duration=20, verbose=True) 
+    totalTime += 20
+    print(f'total time: {totalTime}')
+    print(f'total exp: {totalEXP}')
+    print('*' * 30)
+    invManagement() 
+    Yggdrasil.harvestAll() 
+
