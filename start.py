d982c8ee428ed66f68b34a7fe1b21d5a2d80d69e
matheus <dacachmatheus@gmail.com>
2020-05-28 10:43:37 -0300

    update start script

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
-import subprocess 
+import subprocess
 
-cmd = "xpra start-desktop :1 --start=kwin" 
+#cmd = 'xpra start-desktop :1 --start="xrandr -s 1400x600" --start=kwin --start=alacritty --speaker=off'
+cmd = 'xpra start-desktop :1 --start=kwin --start=alacritty --speaker=off'
 subprocess.run(cmd.split(" "))
 
 
-cmd = "xpra control :1 start alacritty" 
-subprocess.run(cmd.split(" ")) 
-
+#cmd = "xpra control :1 start alacritty"
+#subprocess.run(cmd.split(" "))
