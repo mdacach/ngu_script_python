22b77b9e7b19d868c2ce77ec7f4034f5970462b0
matheus <dacachmatheus@gmail.com>
2020-05-29 13:27:12 -0300

    update

--- itopod_experimental.py
+++ itopod_experimental.py
@@ -2,22 +2,21 @@ from inventory import invManagement
 from features import Itopod, Inventory, Yggdrasil
 from navigation import Navigation
 
-totalTime = 0 
+totalTime = 0
 duration = 5
 while True:
     Navigation.menu('adventure')
     print('*' * 30)
-    Itopod.itopodExperimental(duration=duration) 
-    totalTime += duration 
+    Itopod.itopodExperimental(duration=duration)
+    totalTime += duration
     print(f'total exp: {Itopod.EXP_gained}')
     print(f'total ap: {Itopod.AP_gained}')
     print(f'kills: {Itopod.kills}')
     print(f'total time: {totalTime} minutes')
     print('*' * 30)
-    
+
     Navigation.menu('inventory')
     if Inventory.getEmptySlots() < 10:
-        invManagement() 
-    
-    Yggdrasil.harvestAll() 
+        invManagement()
 
+    Yggdrasil.harvestAll()
