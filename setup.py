5d987864a057b935ae9faad94c97754607c1caaa
matheus <dacachmatheus@gmail.com>
2020-06-03 15:51:37 -0300

    add setup script

---
+++ setup.py
@@ -0,0 +1,62 @@
+import argparse
+
+from helper import Helper
+from navigation import Navigation
+from features import Inventory, GoldDiggers, NGU, Misc
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--preset', '-p',
+                    help='preset of what to run',
+                    default='itopod')
+
+args = parser.parse_args()
+
+
+class Setup:
+    presets = {
+        'itopod': {
+            'loadout': 2,
+            'diggers': ['PP', 'EXP', 'ADVENTURE', 'ENERGY_NGU', 'MAGIC_NGU'],
+            'energy_ngus': ['ADVENTURE_ALPHA', 'DROP_CHANCE'],
+            'magic_ngus': ['YGGDRASIL', 'EXP'],
+            'energy_input': {
+                'amount': 'half',
+                'idle': True,
+                'energy': True,
+            },
+            'magic_input': {
+                'amount': 'half',
+                'idle': True,
+                'energy': False,
+            }
+        }
+    }
+
+    @staticmethod
+    def setup(preset='itopod'):
+
+        preset = Setup.presets[preset]
+        print(preset)
+
+        Helper.init()
+
+        # loadout
+        Navigation.menu('inventory')
+        Inventory.loadout(preset['loadout'])
+
+        # gold diggers
+        Navigation.menu('goldDiggers')
+        GoldDiggers.clearActive()
+        GoldDiggers.activate(preset['diggers'])
+
+        # ngu
+        Navigation.menu('ngu')
+        Misc.inputResource(**preset['energy_input'])
+        NGU.addEnergy(preset['energy_ngus'])
+        Misc.inputResource(**preset['magic_input'])
+        NGU.addMagic(preset['magic_ngus'])
+
+
+if __name__ == '__main__':
+    Setup.setup(args.preset)
