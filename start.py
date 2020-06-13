496f1bb657dc104c285a17fc59c457017c6eacd6
Matheus Cardoso <dacachmatheus@gmail.com>
2020-06-13 08:56:27 -0300

    use kwin for wm

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import subprocess
 
 #cmd = 'xpra start-desktop :1 --start="xrandr -s 1400x600" --start=kwin --start=alacritty --speaker=off'
-cmd = 'xpra start-desktop :100 --start=openbox --start=alacritty --speaker=off'
+cmd = 'xpra start-desktop :100 --start=kwin_x11 --start=alacritty --speaker=off'
 subprocess.run(cmd.split(" "))
 
 
