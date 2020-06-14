cf9ffb2a40aac8e6487d530619c44d153835854c
Matheus Cardoso <dacachmatheus@gmail.com>
2020-06-14 15:37:00 -0300

    OUTDATED

--- start.py
+++ start.py
@@ -1,7 +1,12 @@
 import subprocess
 
+# OUTDATED
+# USING XEPHYR NOW
+
 #cmd = 'xpra start-desktop :1 --start="xrandr -s 1400x600" --start=kwin --start=alacritty --speaker=off'
 cmd = 'xpra start-desktop :100 --start=kwin_x11 --start=alacritty --speaker=off'
+subprocess.run(cmd.split(" "))
+cmd = 'xpra control :100 "xrandr -s 1600x900"'
 subprocess.run(cmd.split(" "))
 
 
